fc73f93ff7a20a93f755dd2a2f606f220d086883
ChenElll <elgarian@g.jct.ac.il>
2024-06-17 15:56:00 +0300

    lo ba li

--- src/navBar.jsx
+++ src/navBar.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Ideas from "./projectsIdea";
 import Account, { CVUpload, Form } from "./account";
+import JobChecker from "./jobChecker";
+import CVImprovement from "./cvImprovement";
+import ToolsLearning from "./toolsLearning";
 import './index.css'; // ייבוא קובץ ה-CSS
 
 export default function NavBar() {
@@ -10,12 +13,17 @@ export default function NavBar() {
             <header>
                 <nav>
                     <Link to="/ideas">Ideas</Link>
+                    <Link to="/jobChecker">Jobs</Link>
+                    <Link to="/toolsLearning">study</Link>
                     <Link to="/account">Me</Link>
                 </nav>
             </header>
             <div className="main-content">
                 <Routes>
                     <Route path="/ideas" element={<Ideas />} />
+                    <Route path="/jobChecker" element={<JobChecker />} />
+                    <Route path="/CVImprovement" element={<CVImprovement />} />
+                    <Route path="/toolsLearning" element={<ToolsLearning />} />
                     <Route path="/account" element={<Account />} />
                     <Route path="/cv-upload" element={<CVUpload />} />
                     <Route path="/form" element={<Form />} />
